65873a3d869690f6c2e5e37f370db3c30f2265e4
Karol Baranowski <karol.baranowski@fastmail.com>
2025-02-24 20:33:13 +0100

    tests for blocktype

--- src/block_type.py
+++ src/block_type.py
@@ -2,13 +2,15 @@ import re
 from enum import Enum
 
 enum  BlockType(Enum):
-    PARAGRAPH = "^#{1-6} "
-    HEADING = ""
-    CODE = ""
+    PARAGRAPH = ""
+    HEADING = r"^(#){1,6} [\w| ]+"
+    CODE = "^```  ```$"
     QUOTE = ""
     UNORDERED_LIST = ""
     ORDERED_LIST + ""
 
     @classsmethod
     def block_to_blocktype(block):
+        if(len(block) == 0):
+            raise ValueError("Block can't be empty")
     
---
+++ src/test_blocktype.py
@@ -0,0 +1,8 @@
+
+import unittest
+
+from textnode import TextNode, TextType
+from leafnode import LeafNode
+
+class TestBlockType(unittest.TestCase):
+
