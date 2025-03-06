5b67106b7e94a747af948cb296518de2f869d682
Karol Baranowski <karol.baranowski@fastmail.com>
2025-03-06 23:56:54 +0100

    Revert "fixed tests for ordered and unordered list" fix was incorrect This reverts commit 6097653019587675a6c3b9937e2b16fdbbc20f60.

--- src/markdown_to_html.py
+++
@@ -1,9 +0,0 @@
-from htmlnode import HTMLNode
-from blocktype import BlockType
-
-class Converter:
-
-    def markdown_to_html(markdown):
-        # 1. Break text to markdown blocks
-        blocks = BlockType.block_to_blocktype(markdown)
-        # 2. 
